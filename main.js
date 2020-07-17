12354cb7b936bee249b66ac79d07aee8477fa8ac
Seshagopal Selvakumar <seshagopalselvakumar@gmail.com>
2020-07-17 15:03:59 +0530

    Added a small tweak in order to not display field when the button is clicked.

--- main.js
+++ main.js
@@ -2,5 +2,9 @@ let housesArray = ["Gryffindor", "Slytherin", "Ravenclaw", "Hufflepuff"];
 startSorting = () => {
   let houseIndex = Math.floor(Math.random() * housesArray.length);
   let houseDisplay = document.querySelector(".house-name");
-  houseDisplay.innerHTML = `You have been sorted into ${housesArray[houseIndex]}!`;
+  houseDisplay.style.display = "none"
+  setTimeout(() => {
+    houseDisplay.innerHTML = `You have been sorted into ${housesArray[houseIndex]}!`;
+    houseDisplay.style.display = "inline-block"    
+  }, 1000);
 };
